e6bfa8bf6fedcd26b919db3e996b1ce9b0c17e97
Hazel Tuxhorn <mx.jeckal@gmail.com>
2020-03-21 10:54:31 -0500

    linked to main page

--- models/symptoms.js
+++ models/symptoms.js
@@ -1,22 +1,17 @@
 module.exports = function (sequelize, DataTypes) {
   var Symptoms = sequelize.define('Symptoms', {
     fever: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
+      type: DataTypes.STRING
     },
     cough: {
       type: DataTypes.STRING
-
     },
     breath: {
       type: DataTypes.STRING
     },
     blueFace: {
-        type: DataTypes.STRING
-      }
-    })
+      type: DataTypes.STRING
+    }
+  })
   return Symptoms
 }
